088749866c262dd81acf97a03db27d13a0f21319
Luca <luca.sotgia@studenti.unicam.it>
2024-03-22 16:57:08 +0100

    Initial Commit

---
+++ src/main/java/it/unicam/cs/LocalDevelopmentPlatform/Ids2024Application.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.LocalDevelopmentPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ids2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ids2024Application.class, args);
+	}
+
+}
