3e67876fb9e11fad8f3f4391450463bc661cdc57
Luca <luca.sotgia@studenti.unicam.it>
2024-03-26 12:37:38 +0100

    PDI espansione

--- src/main/java/it/unicam/cs/LocalDevelopmentPlatform/PuntoDiInteresse.java
+++ src/main/java/it/unicam/cs/LocalDevelopmentPlatform/PuntoDiInteresse.java
@@ -1,5 +1,7 @@
 package it.unicam.cs.LocalDevelopmentPlatform;
 
+import java.util.Objects;
+
 public class PuntoDiInteresse {
 
     private final int id;
@@ -7,8 +9,81 @@ public class PuntoDiInteresse {
     private String nome;
     private TipologiaPunto tipologia;
     private boolean verificato;
+    private boolean segnalato;
     private String descrizione;
+    private String motivoSegnalazione;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PuntoDiInteresse that)) return false;
+        return verificato == that.verificato && segnalato == that.segnalato && Objects.equals(coordinata, that.coordinata) && Objects.equals(nome, that.nome) && tipologia == that.tipologia && Objects.equals(descrizione, that.descrizione) && Objects.equals(motivoSegnalazione, that.motivoSegnalazione);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(coordinata, nome, tipologia);
+    }
 
+    public PuntoDiInteresse(int x, int y, String nome, TipologiaPunto tipologia, String descrizione) {
+        this.coordinata = new Coordinata(x,y);
+        this.nome = nome;
+        this.tipologia = tipologia;
+        this.verificato =false;
+        this.segnalato = false;
+        this.descrizione = descrizione;
+        this.id=hashCode();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Coordinata getCoordinata() {
+        return coordinata;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public TipologiaPunto getTipologia() {
+        return tipologia;
+    }
+
+    public void setTipologia(TipologiaPunto tipologia) {
+        this.tipologia = tipologia;
+    }
+
+    public boolean isVerificato() {
+        return verificato;
+    }
+
+    public void setVerificato(boolean verificato) {
+        this.verificato = verificato;
+    }
+
+    public boolean isSegnalato() {
+        return segnalato;
+    }
+
+    public void setSegnalato(boolean segnalato) {
+        this.segnalato = segnalato;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public String getMotivoSegnalazione() {
+        return motivoSegnalazione;
+    }
+
+    public void setMotivoSegnalazione(String motivoSegnalazione) {
+        this.motivoSegnalazione = motivoSegnalazione;
+    }
 }
